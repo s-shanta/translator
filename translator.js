5a8aac937ec7f14969b04267f79e9d36ff11c3cb
shammy-shanta <45696613+s-shanta@users.noreply.github.com>
2020-11-03 23:40:28 +0600

    Add files via upload

--- translator.js
+++ translator.js
@@ -90,7 +90,7 @@ function runSpeechRecognition() {
   // This runs when the speech recognition service returns result
   recognition.onresult = function(event) {
     var transcript = event.results[0][0].transcript;
-    output.innerHTML = "<b>You've just said : </b> " + transcript ;
+    output.innerHTML = "<b>You have said:</b> " + transcript ;
     output.classList.remove("hide");
     fireBaseInsert(transcript);
   };
